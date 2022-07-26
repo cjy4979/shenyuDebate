e71a6a23c7875cc376bed201b4bce5c29d92772f
cjy4979 <ntchenjiyu@163.com>
2022-07-26 13:05:41 +0800

    最后一次更新

--- src/components/schedule/List.tsx
+++ src/components/schedule/List.tsx
@@ -239,7 +239,9 @@ export default class List extends Component<any, any>  {
     this.setState({ urlOpen: !this.state.urlOpen })
     // var url0 = "https://new.bianlun.online/#/show?rid=9&nub=false&useb=true&off=false&ringBellTime=30&isDisplayTitle=true&custom=true&water=false&colorCode=%23e0ffff%7C%23ffffff%7C%2328769b%7C%23ffffff%7C%23ffffff%7C%23BF2727%7C%23007A9C" +
     // 全黑
-    var url0 = "https://new.bianlun.online/#/show?rid=9&nub=false&useb=true&off=false&ringBellTime=30&isDisplayTitle=true&custom=true&water=false&color=true&colorCode=%23000000%7C%23000000%7C%23000000%7C%23000000%7C%23000000%7C%23000000%7C%23000000" +
+    //var url0 = "https://new.bianlun.online/#/show?rid=9&nub=false&useb=true&off=false&ringBellTime=30&isDisplayTitle=true&custom=true&water=false&color=true&colorCode=%23000000%7C%23000000%7C%23000000%7C%23000000%7C%23000000%7C%23000000%7C%23000000" +
+    //全白
+    var url0 = "https://new.bianlun.online/#/show?rid=9&nub=false&useb=true&off=false&ringBellTime=30&isDisplayTitle=true&custom=true&water=false&color=true&colorCode=%23ffffff%7C%23ffffff%7C%23ffffff%7C%23ffffff%7C%23ffffff%7C%23BF2727%7C%2390cf5b" +
       "&n0=" + encodeURI(this.state.z) +
       "&n1=" + encodeURI(this.state.f) +
       "&t0=" + encodeURI(this.state.topicZ) +
